258f5c08cd970e225bf2f5de59a548f1a8b13c59
pavan <pavankcl>
2020-11-09 17:26:57 +0530

    calculator(pfa wireframe)

--- app/src/main/java/com/example/my_calculatorpfa/MainActivity.java
+++ app/src/main/java/com/example/my_calculatorpfa/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 //import android.view.View;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Stack;
 //import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
@@ -20,7 +25,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     EditText EditText;
     private TextView screen;
     private String display;
-   // float mValueOne, mValueTwo;
+   float mValueOne, mValueTwo;
+    boolean Addition, mSubtract, Multiplication, Division;
    {
        display = " ";
    }
@@ -42,36 +48,64 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         screen = (TextView) findViewById(R.id.textView);
         screen.setText(display);
+        EditText editText = (EditText) findViewById(R.id.phone);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                  //  sendMessage();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+        ArrayList<String> list = new ArrayList<>();
+        //boolean pause = false;
+         Stack s = new Stack();
+
 
         buttonadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    EditText.setText(EditText.getText() + "+");
-                }
 
+                if (EditText == null) {
+                    EditText.setText("");
+                } else {
+                    mValueOne = Float.parseFloat(EditText.getText() + "");
+                    Addition = true;
+                    EditText.setText(null);
+                }
+            }
         });
 
         buttonsub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText.setText(EditText.getText() + "-");
+                mValueOne = Float.parseFloat(EditText.getText() + "");
+                mSubtract = true;
+                EditText.setText(null);
             }
         });
 
         buttonmul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText.setText(EditText.getText() + "*");
+                mValueOne = Float.parseFloat(EditText.getText() + "");
+                Multiplication = true;
+                EditText.setText(null);
             }
         });
 
         buttondiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText.setText(EditText.getText() + "/");
+                mValueOne = Float.parseFloat(EditText.getText() + "");
+                Division = true;
+                EditText.setText(null);
             }
         });
-
 
     }
 
